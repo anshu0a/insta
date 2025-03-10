c6c83cd26c861ab79241f76c68a79db41cba7455
anshu0a <anshukumargupta000@gmail.com>
2025-03-10 15:58:21 +0530

    Fix case sensitivity issue

--- src/jsxFile/home/post-bottom-btn.jsx
+++ src/jsxFile/home/post-bottom-btn.jsx
@@ -30,7 +30,7 @@ export default function PostBottomBtn({ post, userid, type, setCmton, setRespons
 
         try {
             const response = await axios.post(
-                `https://instaserver-knen.onrender.com/dolike/${post._id}`,
+                `http://localhost:8080/dolike/${post._id}`,
                 {},
                 {
                     withCredentials: true,
@@ -69,7 +69,7 @@ export default function PostBottomBtn({ post, userid, type, setCmton, setRespons
 
         try {
             const response = await axios.post(
-                `https://instaserver-knen.onrender.com/savepost/${post._id}`,
+                `http://localhost:8080/savepost/${post._id}`,
                 {},
                 {
                     withCredentials: true,
