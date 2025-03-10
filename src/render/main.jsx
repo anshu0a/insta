13825911312c88f62980d4fa7aeb89a5bca6169a
anshu0a <anshukumargupta000@gmail.com>
2025-03-10 18:11:49 +0530

    coreoptions

--- src/render/main.jsx
+++ src/render/main.jsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import axios from 'axios';
 import './index.css';
+
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
